3d6f29a0ff13a8f01c1f921200305c20440c8aa3
Mitya89 <mitya.melnik89@gmail.com>
2022-07-04 02:18:11 +0300

    DZ_1

--- task_1.4.js
+++ task_1.4.js
@@ -11,26 +11,3 @@ if ((+firstItem && +secondItem) || (typeof(firstItem) === "number" && typeof(sec
 
 
 
-/*
-const firstItem = '5';
-const secondItem = 5;
-let exponent = firstItem ** secondItem;
-
-if ((typeof firstItem === 'number' && typeof secondItem === 'number') && (exponent === exponent)) {
-    console.log(exponent);
-} else { 
-    console.log("Несоответствующий тип данных");
-
-}
-
-
-const firstItem = '5';
-const secondItem = 5;
-let exponent = firstItem ** secondItem;
-
-if ((firstItem === Number(firstItem) && secondItem === Number(secondItem)) && (exponent === exponent)) {
-    console.log(exponent);
-} else { 
-    console.log("Несоответствующий тип данных");
-
-} */
