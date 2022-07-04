f8932675ca6563e79e4c514e52b2a80719695628
Mitya89 <mitya.melnik89@gmail.com>
2022-07-04 14:21:59 +0300

    dz_1

--- task_1.4.js
+++ task_1.4.js
@@ -2,11 +2,20 @@ const firstItem = '5';
 const secondItem = 5;
 let exponent = firstItem ** secondItem;
 
-if ((+firstItem && +secondItem) || (typeof(firstItem) === "number" && typeof(secondItem) === "number") && (exponent === exponent)) {
+
+if ((typeof(firstItem) === "string" || typeof(firstItem) === "number") && (typeof(secondItem) === "number" || typeof(secondItem) === "string")) {
+     let exponent;}
+     if (exponent === exponent) {
      console.log(exponent);
-} else { 
+     } else { 
      console.log("Несоответствующий тип данных");
 }
+
+
+
+
+
+
 
 
 
